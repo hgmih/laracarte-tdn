a9c5584fc4ebbcce6592ec60a010bea09553287d
GTM\hgmih <hichamgmih@gmail.com>
2017-07-05 16:27:21 +0100

    Add page_title helper

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laracarte</title>
+    {{-- <title>{{ isset($title) ? $title . ' | ' : '' }} Laracarte - List of artisans</title> --}}
+    <title>{{ page_title(isset($title) ? $title : '') }}</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
